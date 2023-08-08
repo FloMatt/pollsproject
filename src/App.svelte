16fbba58301dcf2fc0eed065046bc132d6340de3
Matt Wahome <flohr.matt@gmail.com>
2023-08-08 13:28:18 +0300

    Header, footer & tabs components

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,25 @@
 <script>
-	export let name;
+  import Header from "./components/header.svelte";
+  import Footer from "./components/footer.svelte";
+  import Tabs from "./shared/tabs.svelte";
+
+  //tabs
+  let items = ["Current Polls", "Add New Poll"];
+  let activeitem = "Current Polls";
+  const tabChange = (e) => {
+    activeitem = e.detail;
+  };
 </script>
 
+<Header />
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Tabs {activeitem} {items} on:tabChange={tabChange} />
 </main>
+<Footer />
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
